4caba5a63f3b974926a635e4a007d15b62fb0453
jaronschulz <jaronschulz@gmail.com>
2018-05-08 17:41:59 +0200

    created all components and connected them

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
+/* eslint react/prefer-stateless-function: 0 */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Titles from './Components/Titles/Titles';
+import Form from './Components/Form/Form';
+import Weather from './Components/Weather/Weather';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
 }
-
-export default App;
